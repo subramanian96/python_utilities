cc5207698471f46bf5d9397329840b68dd89f267
subramanian96 <subbubat@gmail.com>
2021-03-29 10:35:21 +0000

    Dump yaml to /etc

--- parsetoYaml.py
+++ parsetoYaml.py
@@ -1,10 +1,14 @@
 import json
 import yaml
+import os
 with open('./sample.json') as f:
   data = f.read()
   data = json.loads(data)
 # data is a dictionary of the given json
 print(data)
-f = open('/etc/netplan/sample.yaml', 'w+')
+f = open('./sample.yaml', 'w+')
 yaml.dump(data, f, allow_unicode=True, default_flow_style=False, sort_keys=False)
 f.close()
+password = 'ubuntu'
+command = 'mv ./sample.yaml /etc/netplan/sample.yaml'
+os.popen("sudo -S %s"%(command), 'w').write(password)
